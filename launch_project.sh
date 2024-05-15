0e25a5548371ad5a903282eb0d1463732434aa06
Jona-debug-lang <jona.israel26@outlook.com>
2024-05-15 08:09:08 -0600

    uno

---
+++ launch_project.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Navegar al directorio del workspace de catkin
+cd ~/catkin_ws
+
+# Construir el workspace de catkin
+catkin_make
+
+# Fuente del ambiente de ROS
+source ~/catkin_ws/devel/setup.bash
+
+# Lanzar el nodo del LIDAR
+roslaunch rplidar_ros rplidar_a2m8.launch
+
+# Lanzar el nodo de Hector SLAM
+roslaunch hector_slam_launch tutorial.launch
+
+# Ejecutar el script Python
+rosrun my_python_scripts pose_filter.py
