34d5e8d3f3ca8ac27afd76a6ea8caf78b5bbac62
Jona-debug-lang <jona.israel26@outlook.com>
2024-05-23 18:53:21 +0000

    sd

--- aruco.py
+++ aruco.py
@@ -173,3 +173,66 @@ if __name__ == '__main__':
         pwm_right.stop()
         GPIO.cleanup()
         rospy.loginfo("GPIO cleanup and node shutdown.")
+
+
+
+ubuntu@ubuntu:~/catkin_ws$ rosrun my_python_scripts full3.py
+[INFO] [1716490196.071467]: Ejecutando movimiento recto con detección de ArUco
+[INFO] [1716490196.078089]: Inicio del movimiento recto: Pulsos deseados izq: 643.0600000000001, der: 650.54
+[INFO] [1716490196.083335]: Pulsos actuales: izq: 0, der: 0
+[INFO] [1716490196.102876]: Pulsos actuales: izq: 5, der: 4
+[INFO] [1716490196.122471]: Pulsos actuales: izq: 12, der: 11
+[INFO] [1716490196.147551]: Pulsos actuales: izq: 20, der: 23
+[INFO] [1716490196.158662]: Detected ArUco ID: 2 with size: 29816.0
+[INFO] [1716490196.165166]: Published ArUco ID: 2 pose with x: 0.010743219499180204, y: 0.0350144908512947, z: 0.5077719657368963, quaternion: [0.23045437 0.97245954 0.03476659 0.00212375]
+[INFO] [1716490196.168180]: Pulsos actuales: izq: 27, der: 30
+[INFO] [1716490196.203543]: Pulsos actuales: izq: 41, der: 40
+[INFO] [1716490196.233499]: Pulsos actuales: izq: 50, der: 52
+[INFO] [1716490196.274875]: Pulsos actuales: izq: 67, der: 69
+[INFO] [1716490196.305360]: Pulsos actuales: izq: 81, der: 83
+[INFO] [1716490196.335115]: Pulsos actuales: izq: 96, der: 99
+[INFO] [1716490196.358794]: Pulsos actuales: izq: 109, der: 113
+[INFO] [1716490196.381879]: Pulsos actuales: izq: 121, der: 126
+[INFO] [1716490196.406008]: Pulsos actuales: izq: 133, der: 138
+[INFO] [1716490196.456649]: Pulsos actuales: izq: 156, der: 164
+[INFO] [1716490196.480886]: Pulsos actuales: izq: 169, der: 177
+[INFO] [1716490196.505021]: Pulsos actuales: izq: 180, der: 188
+[INFO] [1716490196.540392]: Pulsos actuales: izq: 191, der: 203
+[INFO] [1716490196.577712]: Pulsos actuales: izq: 199, der: 210
+[INFO] [1716490196.599285]: Pulsos actuales: izq: 210, der: 221
+[INFO] [1716490196.624012]: Pulsos actuales: izq: 218, der: 231
+[INFO] [1716490196.672336]: Pulsos actuales: izq: 244, der: 256
+[INFO] [1716490196.695148]: Pulsos actuales: izq: 256, der: 269
+[INFO] [1716490196.718755]: Pulsos actuales: izq: 266, der: 280
+[INFO] [1716490196.769380]: Pulsos actuales: izq: 294, der: 309
+[INFO] [1716490196.802485]: Pulsos actuales: izq: 306, der: 319
+[INFO] [1716490196.831545]: Pulsos actuales: izq: 324, der: 337
+[INFO] [1716490196.875391]: Pulsos actuales: izq: 345, der: 362
+[INFO] [1716490196.903668]: Pulsos actuales: izq: 360, der: 378
+[INFO] [1716490196.945171]: Pulsos actuales: izq: 376, der: 393
+[INFO] [1716490196.984366]: Pulsos actuales: izq: 402, der: 420
+[INFO] [1716490197.017095]: Pulsos actuales: izq: 418, der: 437
+[INFO] [1716490197.052834]: Pulsos actuales: izq: 438, der: 457
+[INFO] [1716490197.094861]: Pulsos actuales: izq: 459, der: 477
+[INFO] [1716490197.119846]: Pulsos actuales: izq: 471, der: 489
+[INFO] [1716490197.145073]: Pulsos actuales: izq: 484, der: 503
+[INFO] [1716490197.191127]: Pulsos actuales: izq: 504, der: 525
+[INFO] [1716490197.221626]: Pulsos actuales: izq: 518, der: 540
+[INFO] [1716490197.252572]: Pulsos actuales: izq: 535, der: 558
+[INFO] [1716490197.294976]: Pulsos actuales: izq: 556, der: 582
+[INFO] [1716490197.332363]: Pulsos actuales: izq: 576, der: 603
+[INFO] [1716490197.375207]: Pulsos actuales: izq: 592, der: 618
+[INFO] [1716490197.414160]: Pulsos actuales: izq: 607, der: 633
+[INFO] [1716490197.455887]: Pulsos actuales: izq: 623, der: 647
+[INFO] [1716490197.511480]: Movimiento completado: Motores detenidos
+[INFO] [1716490197.538746]: Detected ArUco ID: 2 with size: 29816.0
+[INFO] [1716490197.550042]: ArUco ID: 2, tamaño: 29816.0, posición: [0.01074322 0.03501449 0.50777197], quaternion: [0.23045437 0.97245954 0.03476659 0.00212375]
+[INFO] [1716490197.564610]: Published ArUco ID: 2 pose with x: 0.010743219499180204, y: 0.0350144908512947, z: 0.5077719657368963, quaternion: [0.23045437 0.97245954 0.03476659 0.00212375]
+[INFO] [1716490197.583178]: ArUco ID: 2, tamaño: 29816.0, posición: [0.01074322 0.03501449 0.50777197], quaternion: [0.23045437 0.97245954 0.03476659 0.00212375]
+[INFO] [1716490197.615813]: GPIO cleanup and node shutdown.
+[INFO] [1716490197.701305]: Detected ArUco ID: 2 with size: 30176.5
+[INFO] [1716490197.717051]: Published ArUco ID: 2 pose with x: 0.008555709557462422, y: 0.03570486846913558, z: 0.5038438423506415, quaternion: [ 0.23760678  0.97062718  0.03627    -0.01050654]
+ubuntu@ubuntu:~/catkin_ws$ 
+
+
+
