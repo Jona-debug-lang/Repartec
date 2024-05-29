62f417c2801ae865cbbdb18d8aa584f3635a5b73
Jona-debug-lang <a01275322@tec.mx>
2024-05-29 00:28:23 -0600

    ede

--- FINAL/T2.LIDAR.py
+++ FINAL/T2.LIDAR.py
@@ -242,42 +242,32 @@ def move_straight(distancia_cm, pwm_left_speed, pwm_right_speed, eje_correccion,
     
     if eje_correccion == 'y':
         initial_x = current_x
-        objetivo = initial_x - (distancia_cm / 100.0)  # Distancia negativa para avanzar
+        if direccion == "forward":
+            objetivo = initial_x - (distancia_cm / 100.0)  # Distancia negativa para avanzar
+        else:
+            objetivo = initial_x + (distancia_cm / 100.0)  # Distancia positiva para retroceder
     elif eje_correccion == 'x':
         initial_y = deviation_y
-        objetivo = initial_y + (distancia_cm / 100.0)  # Distancia positiva para avanzar
-    
+        objetivo = initial_y + (distancia_cm / 100.0)  # Distancia positiva para avanzar si 'forward', negativa si 'backward'
+
     rospy.loginfo(f"Inicio del movimiento recto de {distancia_cm} cm")
     
-    while (encoder_left_count < pulsos_deseados_izq or encoder_right_count < pulsos_deseados_der):
+    while True:
         if eje_correccion == 'y':
-            if direccion == "forward":
-                adjust_movement_forward_y(deviation_y)
-                if current_x <= objetivo:
-                    break
-            else:
-                adjust_movement_backward_y(deviation_y)
-                if current_x >= objetivo:
-                    break
+            if (direccion == "forward" and current_x <= objetivo) or (direccion == "backward" and current_x >= objetivo):
+                break
         elif eje_correccion == 'x':
-            if direccion == "forward":
-                adjust_movement_forward_x(current_x)
-                if abs(deviation_y) >= abs(objetivo):
-                    break
-            else:
-                adjust_movement_backward_x(current_x)
-                if abs(deviation_y) >= abs(objetivo):
-                    break
+            if abs(deviation_y) >= abs(objetivo):
+                break
         if encoder_left_count >= pulsos_deseados_izq or encoder_right_count >= pulsos_deseados_der:
             break
-        check_obstacles()  # Verificar obstáculos en cada iteración
+        check_obstacles()
         if should_stop:
             pwm_left.ChangeDutyCycle(0)
             pwm_right.ChangeDutyCycle(0)
             rospy.loginfo("Motores detenidos temporalmente")
-            time.sleep(5)  # Pausa por 5 segundos
-            rospy.loginfo("Revisando nuevamente después de la pausa")
-            for _ in range(20):  # Revisar durante 2 segundos (20 ciclos de 0.1 segundos)
+            time.sleep(5)
+            for _ in range(20):
                 check_obstacles()
                 if should_stop:
                     rospy.loginfo("El obstáculo aún está presente. Pausando nuevamente.")
@@ -287,9 +277,9 @@ def move_straight(distancia_cm, pwm_left_speed, pwm_right_speed, eje_correccion,
             pwm_left.ChangeDutyCycle(pwm_left_speed)
             pwm_right.ChangeDutyCycle(pwm_right_speed)
             rospy.loginfo("Movimiento reanudado")
-        time.sleep(0.01)  # Ajusta este valor según sea necesario
+        time.sleep(0.01)
     
-    pwm_left.ChangeDutyCycle(0)  # Detener los motores
+    pwm_left.ChangeDutyCycle(0)
     pwm_right.ChangeDutyCycle(0)
     rospy.loginfo("Movimiento completado: Motores detenidos")
 
@@ -383,64 +373,3 @@ if __name__ == '__main__':
         pwm_right.stop()
         GPIO.cleanup()
         rospy.loginfo("GPIO cleanup and node shutdown.")
-ubuntu@ubuntu:~$ rosrun my_python_scripts T2.lidar.py
-[INFO] [1716963463.496193]: Received Lidar data: x=-0.004, y=-0.005, theta=-0.00
-[INFO] [1716963463.605346]: Received Lidar data: x=-0.002, y=-0.004, theta=0.00
-[INFO] [1716963463.712999]: Received Lidar data: x=0.002, y=-0.003, theta=0.00
-[INFO] [1716963463.820164]: Received Lidar data: x=-0.004, y=-0.006, theta=-0.00
-[INFO] [1716963463.923850]: Received Lidar data: x=-0.003, y=-0.007, theta=-0.00
-[INFO] [1716963464.014778]: Received Lidar data: x=0.000, y=-0.004, theta=0.00
-[INFO] [1716963464.120180]: Received Lidar data: x=-0.003, y=-0.007, theta=-0.00
-[INFO] [1716963464.235595]: Received Lidar data: x=-0.001, y=-0.007, theta=0.00
-[INFO] [1716963464.338672]: Received Lidar data: x=0.001, y=-0.005, theta=0.00
-[INFO] [1716963464.447306]: Received Lidar data: x=-0.004, y=-0.006, theta=-0.00
-[INFO] [1716963464.558933]: Received Lidar data: x=-0.005, y=-0.007, theta=-0.00
-[INFO] [1716963464.666706]: Received Lidar data: x=-0.003, y=-0.006, theta=-0.00
-[INFO] [1716963464.773288]: Received Lidar data: x=-0.007, y=-0.008, theta=-0.01
-[INFO] [1716963464.924611]: Received Lidar data: x=-0.007, y=-0.009, theta=-0.00
-[INFO] [1716963465.033806]: Received Lidar data: x=-0.006, y=-0.010, theta=-0.00
-[INFO] [1716963465.138336]: Received Lidar data: x=-0.006, y=-0.009, theta=-0.00
-[INFO] [1716963465.236045]: Comando recibido: T2
-[INFO] [1716963465.245580]: Inicio del movimiento recto de 70 cm
-[INFO] [1716963465.252247]: Received Lidar data: x=-0.006, y=-0.010, theta=-0.01
-[INFO] [1716963465.258616]: Adjusting due to Y deviation (forward): -0.010
-[INFO] [1716963465.355818]: Received Lidar data: x=-0.007, y=-0.009, theta=-0.00
-[INFO] [1716963465.461009]: Received Lidar data: x=-0.002, y=-0.006, theta=0.00
-[INFO] [1716963465.567296]: Received Lidar data: x=-0.005, y=-0.007, theta=-0.00
-[INFO] [1716963465.670368]: Received Lidar data: x=-0.001, y=-0.004, theta=0.00
-[INFO] [1716963465.776694]: Received Lidar data: x=-0.004, y=-0.008, theta=-0.01
-[INFO] [1716963465.884445]: Received Lidar data: x=-0.017, y=-0.011, theta=0.01
-[INFO] [1716963465.994362]: Received Lidar data: x=-0.032, y=-0.013, theta=0.00
-[INFO] [1716963466.103605]: Received Lidar data: x=-0.049, y=-0.014, theta=0.00
-[INFO] [1716963466.211955]: Received Lidar data: x=-0.080, y=-0.018, theta=0.01
-[INFO] [1716963466.320295]: Received Lidar data: x=-0.109, y=-0.021, theta=0.01
-[INFO] [1716963466.427278]: Received Lidar data: x=-0.136, y=-0.022, theta=0.01
-[INFO] [1716963466.550856]: Received Lidar data: x=-0.170, y=-0.024, theta=0.01
-[INFO] [1716963466.673233]: Received Lidar data: x=-0.200, y=-0.029, theta=0.01
-[INFO] [1716963466.796305]: Received Lidar data: x=-0.237, y=-0.034, theta=0.02
-[INFO] [1716963466.916197]: Received Lidar data: x=-0.270, y=-0.038, theta=0.02
-[INFO] [1716963467.038120]: Received Lidar data: x=-0.310, y=-0.043, theta=0.02
-[INFO] [1716963467.157901]: Received Lidar data: x=-0.343, y=-0.047, theta=0.02
-[INFO] [1716963467.298248]: Received Lidar data: x=-0.379, y=-0.054, theta=0.02
-[INFO] [1716963467.419713]: Received Lidar data: x=-0.408, y=-0.058, theta=0.02
-[INFO] [1716963467.539472]: Received Lidar data: x=-0.444, y=-0.064, theta=0.03
-[INFO] [1716963467.663863]: Received Lidar data: x=-0.487, y=-0.070, theta=0.03
-[INFO] [1716963467.786628]: Received Lidar data: x=-0.524, y=-0.076, theta=0.03
-[INFO] [1716963467.916971]: Received Lidar data: x=-0.565, y=-0.082, theta=0.04
-[INFO] [1716963468.036006]: Received Lidar data: x=-0.594, y=-0.086, theta=0.04
-[INFO] [1716963468.157017]: Received Lidar data: x=-0.634, y=-0.093, theta=0.05
-[INFO] [1716963468.282853]: Received Lidar data: x=-0.667, y=-0.098, theta=0.04
-[INFO] [1716963468.395678]: Received Lidar data: x=-0.705, y=-0.103, theta=0.05
-[INFO] [1716963468.505394]: Received Lidar data: x=-0.733, y=-0.108, theta=0.05
-[INFO] [1716963468.619185]: Received Lidar data: x=-0.769, y=-0.114, theta=0.05
-[INFO] [1716963468.749143]: Received Lidar data: x=-0.801, y=-0.119, theta=0.05
-[INFO] [1716963468.861985]: Received Lidar data: x=-0.841, y=-0.126, theta=0.05
-[INFO] [1716963468.972022]: Received Lidar data: x=-0.871, y=-0.131, theta=0.06
-[INFO] [1716963469.083382]: Received Lidar data: x=-0.898, y=-0.136, theta=0.06
-[INFO] [1716963469.177836]: Received Lidar data: x=-0.936, y=-0.143, theta=0.06
-[INFO] [1716963469.289221]: Received Lidar data: x=-0.966, y=-0.150, theta=0.06
-[INFO] [1716963469.400394]: Received Lidar data: x=-0.992, y=-0.154, theta=0.07
-[INFO] [1716963469.511590]: Received Lidar data: x=-1.016, y=-0.159, theta=0.07
-[INFO] [1716963469.621053]: Received Lidar data: x=-1.043, y=-0.163, theta=0.07
-[INFO] [1716963469.730237]: Received Lidar data: x=-1.065, y=-0.167, theta=0.07
-^C[INFO] [1716963473.730724]: GPIO cleanup and node shutdown.
